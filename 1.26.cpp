5335a27f19947efd63b66607d62a05f4ef8c7cdc
JINHUITANG <2011464817@qq.com>
2021-01-27 13:00:14 +0800

    十进制整数与二、八、十六进制转化

--- 1.26.cpp
+++ 1.26.cpp
@@ -1,32 +1,52 @@
 /*****************************
 *文件名：1.26.cpp
 *创建人：tjh
-*日期：2021.1.21
-*描述：0~255的十进制转二进制
+*日期：2021.1.27
+*描述：0~4294967295以内的一个十进制整数的进制转换
 ******************************/
 #include<iostream>
 #include<cmath>
 using namespace std;
 int main()
 {
-	int intdec;
+	unsigned int intdec;   //需要进行进制转化的整数
+	int jznum;    //进制数
+	int loopnum;
+	int yunum;
 	
-	cout << "请输入需要转成二进制的一个 0~255 之间的整数 " << endl;
+	cout << "请输入一个0~4294967295以内的一个十进制整数 " << endl;
 	cin >> intdec;
-	for (int i = 1; i < 9; i++)
-	{
-		
-		if (intdec<1)
-		{
-			cout << 0;
-		}
-		else
-		{
-			cout << intdec % 2;   //此时是由低位到高位输出
-		}
-		intdec = intdec / 2;
-	}
+	cout << "请输入要转化的进制位数如2,8,16" << endl;
+	cin >> jznum;
+	
+	if (jznum == 16)
+		loopnum = 8;
 
+	if (jznum == 8)
+		loopnum = 11;
+
+	if (jznum == 2)
+		loopnum = 32;
+
+	for (int i = loopnum - 1; i >= 0; i--)
+	{
+		yunum = intdec / (int)pow(jznum, i) % jznum;
+
+		if (yunum < 10)
+			cout << yunum;
+		else if (yunum == 10)
+			cout.put('A');
+		else if (yunum == 11)
+			cout.put('B');
+		else if (yunum == 12)
+			cout.put('C');
+		else if (yunum == 13)
+			cout.put('D');
+		else if (yunum == 14)
+			cout.put('E');
+		else cout.put('F');
+	}
+				
 	cin.get();
 	cin.get();
 	return 0;
